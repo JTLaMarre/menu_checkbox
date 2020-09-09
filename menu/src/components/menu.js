c2af7ce46b9c2cbed7d8058309371a819045a01c
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-09-09 14:57:40 -0400

    added an onchange event to the checkboxes to grab the name of the menu item.

--- menu/src/components/menu.js
+++ menu/src/components/menu.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormFile from 'react-bootstrap/FormFile'
+import React, {useState,useEffect} from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormFile from 'react-bootstrap/FormFile';
+import items from '../data.json'
+
 
 
 const Menu = (props)=> {
 
+const [menuItems,setmenuItems]=useState(items)
+const [order, setorder]=useState([])
+
+useEffect(()=>{
+    console.log(menuItems)
+})
+
  return(
      <InputGroup className="mb-3">
     <InputGroup.Prepend>
-      <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+      <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={()=>{alert(props.name)}} />
      {props.name}     {props.price}
      </InputGroup.Prepend>
     {/* <FormControl aria-label="Text input with checkbox" /> */}
